467ffc5acdabc7bc4d38fe0499faab402650ce5c
100gLeberwurst <94906749+100gLeberwurst@users.noreply.github.com>
2021-11-23 15:39:25 +0100

    Datentabelle und "Visualisierung" der Daten in R

---
+++ firstTryGit.R
@@ -0,0 +1,5 @@
+datentabelle <- read.csv("C:/Users/Hendrik/Documents/Daten.csv", sep = ";")
+datentabelle
+table(datentabelle$Lieblingsfarbe)
+table(datentabelle$Lieblingstier)
+table(datentabelle$Alter)
